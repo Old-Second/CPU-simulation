4333df42b67ed2a6a77019a4fee7bc23ae992e11
Old-Second <oldsecond2@qq.com>
2024-03-10 16:54:31 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import './App.css'
 import CircuitDiagram from "./Components/CircuitDesigner/CircuitDesigner.tsx";
 import Sidebar from "./Components/Sidebar/Sidebar.tsx";
 import {Layout} from "antd";
-import {DndProvider} from 'react-dnd';
-import {HTML5Backend} from 'react-dnd-html5-backend';
 import {ReactFlowProvider} from "reactflow";
 
 const {Content} = Layout;
@@ -12,16 +10,14 @@ const {Content} = Layout;
 function App() {
   
   return (
-    <DndProvider backend={HTML5Backend}>
-      <Layout>
-        <Sidebar/>
-        <Content>
-          <ReactFlowProvider>
-            <CircuitDiagram/>
-          </ReactFlowProvider>
-        </Content>
-      </Layout>
-    </DndProvider>
+    <Layout>
+      <Sidebar/>
+      <Content>
+        <ReactFlowProvider>
+          <CircuitDiagram/>
+        </ReactFlowProvider>
+      </Content>
+    </Layout>
   )
 }
 

c5c504d9a40f8f68a0c91fb92c82d895e17dad06
Old-Second <oldsecond2@qq.com>
2024-03-10 16:45:05 +0800

    chore: rm

--- src/Components/elements/outputscreen/outputscreen.tsx
+++
@@ -1,9 +0,0 @@
-import './outputscreen.css'
-
-export default function Outputscreen({left, top, text}) {
-    return <div className='outputscreen' style={{
-        position: 'absolute',
-        left: left,
-        top: top,
-    }} >{text}</div>
-}

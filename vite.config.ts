6358cae89dff61780ca1025e58b39c971a5d8023
Old-Second <oldsecond2@qq.com>
2024-06-09 23:37:03 +0800

    chore: change base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd())
   return {
     plugins: [react()],
-    base: '/CPU-simulation/',
+    // base: '/CPU-simulation/',
     server: {
       proxy: {
         '/api': {
